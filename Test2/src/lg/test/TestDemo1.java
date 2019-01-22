d4dbf506cf62f6601fc8bb84dc71074678f0d180
3332601309 <3332601309@qq.com>
2019-01-22 15:15:04 +0800

    terr

--- Test2/src/lg/test/TestDemo1.java
+++ Test2/src/lg/test/TestDemo1.java
@@ -3,5 +3,7 @@ package lg.test;
 public class TestDemo1 {
 		public static void main(String[] args) {
 			System.out.println("dfdfffsafds");
+			System.out.println("dfdfffsafds");
+			System.out.println("dfdfffsafds");
 		}
 }
